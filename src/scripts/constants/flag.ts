7833efa33a9e40fc7e70d5efcf430ad24911912f
Meefki <vitalii.akindinov@gmail.com>
2025-05-30 16:18:47 +0100

    change flags names to constants

--- src/scripts/constants/flag.ts
+++ src/scripts/constants/flag.ts
@@ -6,6 +6,7 @@ export const scope = "interactive-light";
 export const interactiveName = "interactive";
 export const pathName = "path";
 export const tileIdName = "tile-id";
+export const actorIdName = 'actor-id';
 export const interactive = `${flag}.${scope}.${interactiveName}`;
 export const path = `${flag}.${scope}.${pathName}`;
 
