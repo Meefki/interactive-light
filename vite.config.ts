b8180a0deb114fc53de11b8800760351852cc5c5
Meefki <vitalii.akindinov@gmail.com>
2025-06-01 00:43:49 +0100

    edit configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,40 @@
-import path from "path";
-import { defineConfig } from "vite";
+import path from 'path';
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
- build: {
-   sourcemap: "hidden",
-   rollupOptions: {
-     input: "src/interactive-light.ts",
-     output: {
-        dir: path.resolve("./dist"),
-        entryFileNames: "interactive-light.js"
-     },
-     external: [
-      "socketlib"
-     ]
-   },
- }
+    build: {
+        outDir: path.resolve('./dist'),
+        emptyOutDir: true,
+        sourcemap: undefined,
+        rollupOptions: {
+            input: 'src/interactive-light.ts',
+            output: {
+                dir: path.resolve('./dist/src'),
+                entryFileNames: 'interactive-light.js',
+            },
+        },
+    },
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'module.json',
+                    dest: './'
+                },
+                {
+                    src: 'LICENSE',
+                    dest: './'
+                },
+                {
+                    src: 'readme.md',
+                    dest: './'
+                },
+                {
+                    src: 'lang/*',
+                    dest: './lang/'
+                }
+            ]
+        })
+    ]
 });
