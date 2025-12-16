0c00e103ec046b06585c9c780a63914dddc96088
Meefki <vitalii.akindinov@gmail.com>
2025-12-16 02:15:01 +0000

    add prefab selection

--- src/scripts/prefab-window/light-prefab-browser.js
+++ src/scripts/prefab-window/light-prefab-browser.js
@@ -1,3 +1,5 @@
+import { Logger } from "../utils/logger.js";
+
 const {
   ApplicationV2,
   HandlebarsApplicationMixin
@@ -8,10 +10,13 @@ export class LightPrefabBrowserV2 extends HandlebarsApplicationMixin(
 ) {
     static DEFAULT_OPTIONS = {
         id: "light-prefabs",
+        actions: {
+            selectPrefab: LightPrefabBrowserV2.__onPrefabClick
+        },
         classes: ["prefab-browser-window"],
         position: { 
-            height: 670,
-            width: 570,
+            height: 675,
+            width: 590,
             left: 105,
             top: 60
         },
@@ -28,10 +33,10 @@ export class LightPrefabBrowserV2 extends HandlebarsApplicationMixin(
         main: { template: "modules/interactive-light/dist/templates/light-prefab-browser/main.hbs" }
     };
 
-    activePrefab = null;
-    search = "";
-    allTags = [];
-    activeTags = [];
+    __activePrefab = null;
+    __search = "";
+    __allTags = [];
+    __activeTags = [];
 
     async _prepareContext(context, options) {
         const _context = {
@@ -41,55 +46,80 @@ export class LightPrefabBrowserV2 extends HandlebarsApplicationMixin(
         return _context;
     }
 
+    static __onPrefabClick(event, target) {
+        Logger.log("selectPrefab:", event, target);
+        if (this.__activePrefab === target.dataset.id) {
+            this.__activePrefab = null;
+            target.checked = false;
+            // target.dataset.checked = false;
+        } else {
+            this.__activePrefab = target.dataset.id;
+            target.checked = true;
+            // target.dataset.checked = true;
+        }
+
+        Logger.log("selectPrefab.__activePrefab:", this.__activePrefab);
+    }
+
     __mockPrefabs() {
         return [
             {
-                preview: '',
+                id: 'light-1',
+                preview: 'assets/ForgottenAdventures/!Core_Settlements/Lightsources/Campfires_and_Firepits/Campfire_Embers_C1_1x1.webp',
                 name: 'test1',
-                tags: ['tag1', 'tag2', 'tag3']
+                tags: ['tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6', 'tag7', 'tag8', 'tag9', 'tag10']
             },
             {
-                preview: '',
+                id: 'light-2',
+                preview: 'assets/ForgottenAdventures/!Core_Settlements/Lightsources/Campfires_and_Firepits/Campfire_Ash_C1_2x2.webp',
                 name: 'test2',
                 tags: ['tag1', 'tag2', 'tag3']
             },
             {
-                preview: '',
+                id: 'light-3',
+                preview: 'assets/ForgottenAdventures/!Core_Settlements/Lightsources/Campfires_and_Firepits/Campfire_Stone_Redrock_A1_1x1.webp',
                 name: 'test3',
                 tags: ['tag1', 'tag2', 'tag3']
             },
             {
-                preview: '',
+                id: 'light-4',
+                preview: 'assets/ForgottenAdventures/!Core_Settlements/Lightsources/Campfires_and_Firepits/Campfire_Stone_Redrock_A1_Lit_1x1.webp',
                 name: 'test4',
                 tags: ['tag1', 'tag2', 'tag3']
             },
             {
-                preview: '',
+                id: 'light-5',
+                preview: 'assets/ForgottenAdventures/!Core_Settlements/Lightsources/Campfires_and_Firepits/Campfire_Wood_Ashen_Lit_A1_1x1.webp',
                 name: 'test5',
                 tags: ['tag1', 'tag2', 'tag3']
             },
             {
-                preview: '',
+                id: 'light-6',
+                preview: 'assets/ForgottenAdventures/!Core_Settlements/Lightsources/Lamps/Lamp_Street_Double_Metal_Brass_C_1x1.webp',
                 name: 'test6',
                 tags: ['tag1', 'tag2', 'tag3']
             },
             {
-                preview: '',
+                id: 'light-7',
+                preview: 'assets/ForgottenAdventures/!Core_Settlements/Lightsources/Lamps/Lamp_Street_Double_Metal_Brass_J_1x1.webp',
                 name: 'test7',
                 tags: ['tag1', 'tag2', 'tag3']
             },
             {
-                preview: '',
+                id: 'light-8',
+                preview: 'assets/ForgottenAdventures/!Core_Settlements/Lightsources/Lamps/Mage_Light_Red_1x1.webp',
                 name: 'test8',
                 tags: ['tag1', 'tag2', 'tag3']
             },
             {
-                preview: '',
+                id: 'light-9',
+                preview: 'assets/ForgottenAdventures/!Core_Settlements/Lightsources/Lamps/Mage_Light_Blue_1x1.webp',
                 name: 'test9',
                 tags: ['tag1', 'tag2', 'tag3']
             },
             {
-                preview: '',
+                id: 'light-10',
+                preview: 'assets/ForgottenAdventures/!Core_Settlements/Lightsources/Torches_and_Sconces/Sconce_B_1x1.webp',
                 name: 'test10',
                 tags: ['tag1', 'tag2', 'tag3']
             }
